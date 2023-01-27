48b2bdcedbf86de6f270f604396ab000041e1204
geekbrains <maksimov0904@icloud.com>
2023-01-28 00:10:33 +0500

    add EX2

---
+++ GEEK BRAINS/С#/HWork26.01/EX2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите число 1 ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int B = int.Parse(Console.ReadLine());
+if (A>B)
+{
+    Console.WriteLine("большее число " + A);
+}
+else
+{
+    Console.WriteLine("большее число " + B);
+}
